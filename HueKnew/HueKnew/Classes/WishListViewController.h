083aeeb16ea69bb425ed7980cd0b16fcdf1fd235
Gilad Shai <shai.gilad@gmail.com>
2013-04-26 01:11:23 -0400

    set navigation to wish list

---
+++ HueKnew/HueKnew/Classes/WishListViewController.h
@@ -0,0 +1,13 @@
+//
+//  WishListViewController.h
+//  HueKnew
+//
+//  Created by Gilad Shai on 4/25/13.
+//  Copyright (c) 2013 Shai, Gilad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WishListViewController : UITableViewController
+
+@end
