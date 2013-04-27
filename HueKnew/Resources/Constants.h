2e43f3ea7916f5f3ab0663be9353baea753bd78d
Gilad Shai <shai.gilad@gmail.com>
2013-04-27 00:41:18 -0400

    add constants.h and auto slide after receiving callback

---
+++ HueKnew/Resources/Constants.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  HueKnew
+//
+//  Created by Gilad Shai on 4/26/13.
+//  Copyright (c) 2013 Shai, Gilad. All rights reserved.
+//
+
+#ifndef HueKnew_Header_h
+#define HueKnew_Header_h
+
+
+
+#endif
